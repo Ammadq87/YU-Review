22835676eb40e723729f4165be5a98ecb1ba7bb8
Ammad Qureshi <77247515+Ammadq87@users.noreply.github.com>
2023-04-30 15:50:48 -0400

    Added routing links for login and signup (#4)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
+import Main from './components/Main.jsx'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Navbar/>
-      <Banner/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />}/>
+        </Routes>
+      </Router>
+
+      
     </div>        
   )
 }
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar.jsx';
+import Banner from './Banner.jsx';
+
+export default function Main () {
+    return (
+        <div>
+            <Navbar/>
+            <Banner/>
+        </div>
+    )
+}
