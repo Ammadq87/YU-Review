ac98983339176110a2a12b407a1188839090f971
Ammad <ammadq87@my.yorku.ca>
2023-05-05 00:20:16 -0400

    Added review component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,11 +1,14 @@
 import Navbar from './Navbar.jsx';
 import Banner from './Banner.jsx';
+import Review from './Review.jsx';
 
 export default function Main () {
+
     return (
         <div>
             <Navbar/>
             <Banner/>
+            <Review/>
         </div>
     )
 }
