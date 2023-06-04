7be90adcd16df97349c9aae62f91debcdddee6f0
Ammad <ammadq87@my.yorku.ca>
2023-06-04 18:41:24 -0400

    css changes

--- client/src/Pages/Login/Login.jsx
+++ client/src/Pages/Login/Login.jsx
@@ -1,37 +1,22 @@
-import Banner from '../../components/Banner/Banner';
-import Navbar from '../../components/Navbar/Navbar.jsx';
-import Form from '../../components/Form/Form';
 import './Login.css'
-import { useState } from 'react';
-import axios from 'axios';
 
 export default function Login(props) {
 
-    const bannerData = {
-        title: props?.data?.title ? props.data.title : 'Login', // Temp
-        subtitle: props?.data?.subtitle ? props.data.subtitle : '',
-        extend: false,
-        favouritable: false
-    };
-
-    const fields = [
-        {label: 'Email', type: 'email'}, 
-        {label: 'Password', type: 'password'},
-    ]
-
     return (
         <div className="loginPage">
-            <Banner data={bannerData}/>
-            <div className="loginForm">
-                
-                <Form data={{'fields': fields, 'endPoint': '/users/login/', 'type': 'Login'}}/>
+            <h1>Login</h1>
+            <input className="textInput" placeholder='Email' type='mail'></input>
+            <input className="textInput" placeholder='Password' type='password'></input>
 
-                <div className='image'>
-                    <img src="https://yorkulions.ca/images/2020/5/20/10199_YUAT_Lions_RGB_black_lion_black_YU.png" alt="" />
+            <div className="loginLinks">
+                <div className="link">
+                    <a className="pageLink" href='/' style={{color: '#3876F9'}}>Forgot Password?</a>
                 </div>
-
+                <div className="link" style={{textAlign: 'right'}}>
+                    <a className="pageLink" href='/SignUp' style={{color: '#3876F9'}}>Sign Up</a>
+                </div>
             </div>
-
+            <button className='selectedBtn'>Login</button>
         </div>
     )
 }
